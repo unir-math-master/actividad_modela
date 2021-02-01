c84447de05cb8537d6811892ef229d89296d5277
Erick Díaz Saborio <ewdiazsa@gmail.com>
2021-01-31 18:36:26 -0600

    updated a,b,c values for random number generator

--- gen_var_compo.m
+++ gen_var_compo.m
@@ -1,17 +1,20 @@
 function [u] = gen_var_compo()
 global xn;
+global xn2;
 
-[R1,xn] = gen(16,4,75,xn);
-[R2, xn] = gen(16,4,75,xn);
+%[R1,xn] = gen(3,1,1048576, xn);
+%[R2, xn2] = gen(3,1,1048576,xn2);
+R1 = rand();
+R2 = rand();
 
 if R1 <= 1/4
-    fx=pi*R2
-elseif (1/4< R1) && (R1<=1/2)
-    fx= 4* asin((sqrt(2)/2)*R2)
-elseif (1/2<R1) && (R1<=1)
-    fx=2*acos(-1*R2)
+    fx=pi*R2;
+elseif (R1>1/4) && (R1<=1/2)
+    fx=4*asin((sqrt(2)/2)*R2);
+elseif (R1>1/2) && (R1<=1)
+    fx=2*acos(-1*R2);
 end
 
-u=fx
+u=fx;
 end
 
